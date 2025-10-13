3293aa689f70ad710617193471b6a367ec203150
Arun4486 <arunkatiyar2073@gmail.com>
2025-10-13 14:53:38 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,22 @@
+console.log("hi there");
+require('dotenv').config()
+const express = require('express')
+const app = express()
+const port = 4000
+
+app.get('/',(req, res)=>{
+    res.send("hi there")
+})
+
+app.get('/insta', (req,res)=>{
+    res.send("my insta handle")
+})
+
+app.listen(process.env.PORT, ()=>{
+    console.log(`listening app on ${port}`);
+    
+})
+
+app.get('/youtube', (req,res)=>{
+    res.send('<h1>hahaha</h1>')
+})
