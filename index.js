d8d20f16aaa5896a6fcaaae3bd1add14a54d2a75
Arun Katiyar <arun.katiyar@ip-192-168-1-72.eu-central-1.compute.internal>
2025-12-18 11:25:36 +0530

    first commit remotly

--- index.js
+++ index.js
@@ -1,22 +1,23 @@
+console.log("cloned successfully");
+
 console.log("hi there");
-require('dotenv').config()
-const express = require('express')
-const app = express()
-const port = 4000
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const port = 4000;
 
-app.get('/',(req, res)=>{
-    res.send("hi there")
-})
+app.get("/", (req, res) => {
+  res.send("hi there");
+});
 
-app.get('/insta', (req,res)=>{
-    res.send("my insta handle")
-})
+app.get("/insta", (req, res) => {
+  res.send("my insta handle");
+});
 
-app.listen(process.env.PORT, ()=>{
-    console.log(`listening app on ${port}`);
-    
-})
+app.listen(process.env.PORT, () => {
+  console.log(`listening app on ${port}`);
+});
 
-app.get('/youtube', (req,res)=>{
-    res.send('<h1>hahaha</h1>')
-})
+app.get("/youtube", (req, res) => {
+  res.send("<h1>hahaha</h1>");
+});
